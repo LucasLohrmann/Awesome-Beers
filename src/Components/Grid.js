3a14b4548f2b230f2e81ab16bacc87f1b78b30e6
Lucas Lohrmann <lucaslohrmann@gmail.com>
2019-06-02 14:03:39 -0300

    Pagination

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -3,30 +3,49 @@ import axios from 'axios';
 import '../Css/Grid.css';
 
 import GridItem from './GridItem'
+import Paginator from './Paginator'
 
 export default class Grid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            paginatedData: []
         }
+
+        this.displayPage = this.displayPage.bind(this);
    }
 
    componentDidMount(){
     axios.get(`https://api.punkapi.com/v2/beers`)
       .then(res => {
+          debugger;
         const data = res.data;
-        this.setState({ data });
+        this.setState({ data }, () => this.displayPage(1, 0, 4));
       })
+    }
+
+    displayPage(pageNumber, firstItemIndex, lastItemIndex){
+        debugger;
+        let paginatedData = this.state.data.slice(firstItemIndex, lastItemIndex);
+
+        this.setState({
+            paginatedData: paginatedData
+        });
     }
 
     render() {
         return (
-            this.state.data.map((item, i) => {
-                return(
-                    <GridItem key={i} item={item} />
-                )
-            })
+            <div>
+                {
+                    this.state.paginatedData.map((item, i) => {
+                        return(
+                            <GridItem key={i} item={item} />
+                        )
+                    })
+                }
+                <Paginator itemsPerPage={4} itemTotalCount={this.state.data.length} displayPage={(pageNumber, firstItemIndex, lastItemIndex) => this.displayPage(pageNumber, firstItemIndex, lastItemIndex)}/>
+            </div>
         )
     }
 }
--- src/Components/Paginator.js
+++ src/Components/Paginator.js
@@ -1,13 +1,65 @@
 import React from 'react';
 import '../Css/Paginator.css';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 export default class Paginator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            currentPage: 1,
+            pageCount: 0,
+            pageNumbers: [],
+            itemTotalCount: 0,
+            itemsPerPage: 0,
+            firstItemIndex: 0,
+            lastItemIndex: 0
+        }
+        this.handlePageSelection = this.handlePageSelection.bind(this);
+        this.setCurrentPage = this.setCurrentPage.bind(this);
    }
+
+   componentWillReceiveProps(nextProps){
+       debugger;
+       let pageCount = Math.ceil(nextProps.itemTotalCount / nextProps.itemsPerPage);
+       let pageNumbers = [];
+       for (let i = 1; i <= pageCount; i++) {
+        pageNumbers.push(i);
+       }
+
+        this.setState({
+            itemsPerPage: nextProps.itemsPerPage,
+            itemTotalCount: nextProps.itemTotalCount,
+            pageCount: pageCount,
+            pageNumbers: pageNumbers
+        });
+   }
+
+   setCurrentPage(pageNumber){
+       debugger;
+       this.setState({
+           currentPage: pageNumber
+       }, () => this.handlePageSelection());
+   }
+
+   handlePageSelection(){
+       debugger;
+    let lastItemIndex = (this.state.currentPage * this.state.itemsPerPage);
+    let firstItemIndex = (lastItemIndex - this.state.itemsPerPage);
+
+        this.props.displayPage(this.state.currentPage, firstItemIndex, lastItemIndex);
+   }
+
     render() {
          return (
-            <div></div>
+            <div>
+                {
+                    this.state.pageNumbers.map((number, i) => {
+                        return(
+                            <button id={number} key={i} onClick={(e) => this.setCurrentPage(e.target.id)}>{number}</button>
+                        );
+                    })
+                }
+            </div>
          );
     }
 }
